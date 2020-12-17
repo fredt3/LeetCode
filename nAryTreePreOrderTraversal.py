8a6681e4b6ffc89d10573f2e8a4230fae26d4a1c
freddyt3 <fred.tidwell.03@gmail.com>
2020-12-17 18:15:41 -0500

    Added 589. N-ary Tree Preorder Traversal

---
+++ nAryTreePreOrderTraversal.py
@@ -0,0 +1,23 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+
+class Solution:
+    def preorder(self, root):
+        res = []
+        if root == None:
+            return res
+        stack = [root]
+
+        while stack:
+            node = stack.pop()
+            res.append(node.val)
+            for child in node.children[::-1]:
+                stack.append(child)
+
+        return res
